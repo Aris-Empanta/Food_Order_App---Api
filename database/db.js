1b016c7d9175d941b06082e6cc550e78b0db4255
Aris Empanta <eams220891@gmail.com>
2022-08-07 02:29:25 +0300

    separating mysql configuraton file

---
+++ database/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'sql11.freemysqlhosting.net',
+    user: 'sql11511034',
+    password: '7mJh3f3LPl',
+    database: 'sql11511034',
+    port: "3306"
+  })
+
+connection.connect((err) => { if(err) {
+                              console.log(err)
+                            } 
+                            console.log("connected to restaurant database")})  
+
+module.exports = connection
--- server.js
+++ server.js
@@ -1,37 +1,21 @@
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 5000
-const mysql = require('mysql2')
 const cors = require("cors")
+const db = require('./database/db')
 
 app.use(cors())
-
-const connection = mysql.createConnection({
-    host: 'sql11.freemysqlhosting.net',
-    user: 'sql11511034',
-    password: '7mJh3f3LPl',
-    database: 'sql11511034',
-    port: "3306"
-  })
-
-connection.connect((err) => { if(err) {
-                              console.log(err)
-                            } 
-                            console.log("connected to restaurant database")})  
-
 
 app.get("/", (req, res) => {
     
     let sql = "SELECT * FROM food"
 
-    connection.query(sql, (err, rows) => {
+    db.query(sql, (err, rows) => {
       if(err){
         console.log(err)
       }
       res.send(rows)
     })
-
-
 })
 
 app.listen(port, () => console.log(`App is listening on port ${port}`))
