fb0ee4c451a2a5c48d1fefbb0f8a6c117bc87f5b
Aris Empanta <eams220891@gmail.com>
2022-08-07 17:43:21 +0300

    configuring post requests

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require("cors")
 const db = require('./database/db')
+const bodyParser = require("body-parser")
 
 app.use(cors())
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
 
 app.get("/", (req, res) => {
     
@@ -15,6 +18,22 @@ app.get("/", (req, res) => {
         console.log(err)
       }
       res.send(rows)
+    })
+})
+
+app.post("/foods", (req, res) => {
+    
+
+    let category = req.body.category
+
+    let sql = "ALTER TABLE food ADD " + category +" VARCHAR(20) NOT NULL"
+
+    db.query(sql, (err, rows) => {
+      if(err){
+        console.log(err)
+      } else{
+        res.send(rows)
+      }
     })
 })
 
