981656219649fc33e64525225b1bcca04feca5f9
Aris Empanta <eams220891@gmail.com>
2022-08-09 20:02:03 +0300

    daily updates

--- server.js
+++ server.js
@@ -3,11 +3,35 @@ const app = express()
 const port = process.env.PORT || 5000
 const cors = require("cors")
 const db = require('./database/db')
-const bodyParser = require("body-parser")
+const multer = require("multer")
+//Multer middleware
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
+
+/*let sql= `CREATE TABLE Products(
+              Category varchar(255),
+              Name varchar(255),
+              Currency varchar(255),
+              Quantity int, 
+              Delivery_price int,
+              Take_away_price int,
+              Description  varchar(255),
+              Image_name varchar(255)
+          )`
+
+db.query(sql)*/
+const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, "uploads")
+      },
+      filename: (req, file, cb) => {
+        cb(null, "tasos")
+      }
+})
+
+const upload = multer({storage: storage})
 
 app.get("/", (req, res) => {
     
@@ -21,14 +45,23 @@ app.get("/", (req, res) => {
     })
 })
 
-app.post("/foods", (req, res) => {
+app.post("/products", upload.single("image"),  (req, res) => {
     
 
     let category = req.body.category
+    let name = req.body.name
+    let currency = req.body.currency
+    let quantity = req.body.quantity
+    let deliveryPrice = req.body.deliveryPrice
+    let takeAwayPrice = req.body.takeAwayPrice
+    let description = req.body.description
+    let image = req.body.image
+   
+    console.log(req.file, req.body)
+    let sql = `INSERT INTO Products 
+               VALUES (?, ?, ?, ?, ?, ?,  ?, ?)`
 
-    let sql = "ALTER TABLE food ADD " + category +" VARCHAR(20) NOT NULL"
-
-    db.query(sql, (err, rows) => {
+    db.query(sql,[category, name, currency, quantity, deliveryPrice, takeAwayPrice, description, image ] , (err, rows) => {
       if(err){
         console.log(err)
       } else{
