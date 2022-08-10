4c1e5076f16710383cee76b877a65c112acce731
Aris Empanta <eams220891@gmail.com>
2022-08-10 04:08:45 +0300

    config

--- server.js
+++ server.js
@@ -4,30 +4,18 @@ const port = process.env.PORT || 5000
 const cors = require("cors")
 const db = require('./database/db')
 const multer = require("multer")
-//Multer middleware
 
 app.use(cors())
 app.use(express.urlencoded({extended: true}));
 app.use(express.json())
 
-/*let sql= `CREATE TABLE Products(
-              Category varchar(255),
-              Name varchar(255),
-              Currency varchar(255),
-              Quantity int, 
-              Delivery_price int,
-              Take_away_price int,
-              Description  varchar(255),
-              Image_name varchar(255)
-          )`
 
-db.query(sql)*/
 const storage = multer.diskStorage({
       destination: (req, file, cb) => {
         cb(null, "uploads")
       },
       filename: (req, file, cb) => {
-        cb(null, "tasos")
+        cb(null, Date.now() + "-" + file.originalname)
       }
 })
 
