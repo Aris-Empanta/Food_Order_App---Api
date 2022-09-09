b35651fd6537f311757cd91a25eca3da997478d8
Aris Empanta <eams220891@gmail.com>
2022-09-09 17:14:54 +0300

    Completed verification code configuration

--- routes/mailConfirmation.js
+++ routes/mailConfirmation.js
@@ -1,12 +1,29 @@
 const express = require("express")
 const router = express.Router()
-const mail = require("nodemailer")
+const nodemailer = require("nodemailer")
+const dotenv = require('dotenv').config()
 
 router.post("/", (req, res) => {
 
     let mail = req.body.mail
+    //Nodemailer accept only string in its configurations
+    let verificationCode = String(req.body.verificationCode)
 
-    console.log(mail)
+    let transporter = nodemailer.createTransport({
+      service: 'outlook',
+      auth: {
+        user: process.env.ADMIN_MAIL,
+        pass: process.env.ADMIN_PASSWORD
+      }
+    })
+
+    transporter.sendMail({
+        from: process.env.ADMIN_MAIL ,
+        to: mail, 
+        subject: "YOUR EMAIL VERIFICATION", 
+        text: verificationCode,      
+      }, (err) => { if(err) console.log(err)
+                    console.log("success")})       
 })
 
 module.exports = router;
