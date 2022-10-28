8e7983713738f0421816494fcddf9081e2888363
Aris Empanta <eams220891@gmail.com>
2022-10-28 22:58:48 +0300

    Removed email confirmation functionality

--- routes/nodemailer.js
+++ routes/nodemailer.js
@@ -6,30 +6,6 @@ const nodemailer = require("nodemailer")
 //emails you can send per minute/hour. If it were for real business
 //purposes, we should use a paied domain.
 
-//The route to send verification code to the customer
-router.post("/confirm", (req, res) => {
-
-    let mail = req.body.mail
-    //Nodemailer accept only string in its configurations
-    let verificationCode = String(req.body.verificationCode)
-
-    let transporter = nodemailer.createTransport({
-      service: 'outlook',
-      auth: {
-        user: process.env.ADMIN_MAIL,
-        pass: process.env.ADMIN_PASSWORD
-      }
-    })
-
-    transporter.sendMail({
-        from: process.env.ADMIN_MAIL ,
-        to: mail, 
-        subject: "YOUR EMAIL VERIFICATION", 
-        text: verificationCode + "\n\n This code will be active for the next five minutes.",      
-      }, (err) => { if(err) console.log(err)})       
-})
-
-
 //The route that a customer uses to send a mail to admin
 router.post("/customer-form", (req, res) => {
 
