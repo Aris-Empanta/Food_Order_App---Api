6fb98c713214ea5e4ec583430678dda9d09ed558
Aris Empanta <eams220891@gmail.com>
2022-11-05 14:18:54 +0200

    ome changes in .env file

--- database/db.js
+++ database/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2')
 require("dotenv").config()
 
 const connection = mysql.createConnection({
-    host: "eris.myip.gr",
-    user: "arisdb_snackbar_data",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: "arisdb_snackbar_data",
-    port: "3306" 
+    database: process.env.DB_NAME,
+    port: process.env.PORT 
   })
 
 connection.connect((err) => { if(err) {
