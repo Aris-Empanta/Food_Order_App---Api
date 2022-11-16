7b8c83098096f1abfd9f232a164dcb76ad69032a
Aris Empanta <eams220891@gmail.com>
2022-11-16 16:37:32 +0200

    Resetting serverHost variable

--- variables/variables.js
+++ variables/variables.js
@@ -1,4 +1,4 @@
-//In the serverHost variable, we keep the hist address, which is needed in many parts of our
+//In the serverHost variable, we keep the host address, which is needed in many parts of our
 //code. So that if we change host, instead of changing it in every single part, we change only
 //the variable below.
 module.exports = {
